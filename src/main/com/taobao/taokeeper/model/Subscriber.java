fd9bee8d8ac6ad5a6d15d018a25c2c5252980850
thinkhejie <thinkhejie@gmail.com>
2015-11-27 18:06:50 +0800

    initial commit

---
+++ src/main/com/taobao/taokeeper/model/Subscriber.java
@@ -0,0 +1,137 @@
+package com.taobao.taokeeper.model;
+
+import static com.taobao.taokeeper.common.constant.SystemConstant.DELAY_SECS_OF_TWO_SERVER_ALIVE_CHECK_ZOOKEEPER;
+import static common.toolkit.java.constant.EmptyObjectConstant.EMPTY_STRING;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.I0Itec.zkclient.IZkDataListener;
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.zookeeper.CreateMode;
+
+/**
+ * ��˵��: 
+ * @author yinshi.nc
+ */
+public class Subscriber {
+
+	private static Log log = LogFactory.getLog(Subscriber.class); // �����־
+
+	private int okTimes = 0;
+	private String serverList;
+	private String path;
+	private ZkClient zkClient = null;
+	private int maxDelaySecsForNotify = 1;
+	//�������������ֱ������һ����server�ύ�ĸ�����ݺ�����õ�����ݣ�����ֵһ��˵�����
+	private String lastedUpdateToServer = EMPTY_STRING;
+
+	public void setLastedUpdateToServer(String lastedUpdateToServer) {
+		this.lastedUpdateToServer = lastedUpdateToServer;
+	}
+
+	public Subscriber(String serverList, String path, int maxDelaySecsForNotify) {
+		this.serverList = serverList;
+		this.path = path;
+		zkClient = new ZkClient(serverList, 5000);
+		this.maxDelaySecsForNotify = maxDelaySecsForNotify;
+		//��������
+		this.mointorData(path);
+	}
+
+	/**
+	 * ��� parentPath Ŀ¼��������ݵı��
+	 * ע��, ����ļ���������, ֻ�ܼ����Լ�.
+	 * @param parentPath
+	 */
+	private void mointorData(String path) {
+		zkClient.subscribeDataChanges(path, new IZkDataListener() {
+			public void handleDataChange(String dataPath, Object data) throws Exception {
+				if (lastedUpdateToServer.equalsIgnoreCase(data.toString())) {
+					okTimes++;
+					return;
+				}
+			}
+
+			public void handleDataDeleted(String dataPath) throws Exception {
+				//ingore
+			}
+		});
+	}
+
+	/**
+	 * ����Ƿ����鷽���ǣ���һ��ip������ݣ�Ҫ����ָ��ʱ�����յ�֪ͨ����ȡ��ȷ��ݡ��������������3�Σ�������һ��ʧ�ܼ�ʧ�ܡ�
+	 */
+	public boolean checkIfAlive() {
+		createNodeNotExist(this.path);
+		for (int i = 0; i < 3; i++) {
+			this.lastedUpdateToServer = this.serverList + System.currentTimeMillis();
+			try {
+				this.updateData(this.lastedUpdateToServer);
+				Thread.sleep(1000 * this.maxDelaySecsForNotify);//ָ��ʱ����
+			} catch (InterruptedException e) {
+				//ingore
+			} catch (Exception e) {
+				return false;
+			}
+		}
+
+		return okTimes == 3;
+	}
+
+	public void close() {
+		this.zkClient.close();
+		this.zkClient = null;
+	}
+
+	/** �����ڣ������ڵ� */
+	private void createNodeNotExist(String defaultData) {
+		if (!zkClient.exists(this.path)) {
+			zkClient.create(this.path, this.serverList + System.currentTimeMillis(), CreateMode.PERSISTENT);
+		}
+	}
+
+	/**�������*/
+	private void updateData(String newData) throws Exception {
+		zkClient.writeData(this.path, newData);
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		List<String> serverList = new ArrayList<String>();
+		serverList.add("10.232.37.129:2181");
+		serverList.add("10.232.37.128:2181");
+		serverList.add("10.232.37.126:2181");
+		serverList.add("10.13.44.47:2181");
+
+		while (true) {
+			for (String server : serverList) {
+
+				Subscriber sub = null;
+				try {
+					sub = new Subscriber(server, "/yinshi.test", 1);
+					//�ж�һ���ڵ��Ѿ����ˣ��������μ���ʧ�ܡ�
+					if (!sub.checkIfAlive()) {
+						if (!sub.checkIfAlive()) {
+							log.error(server + "����Լ��ʧ��");
+						} else {
+							log.info(server + "����Լ��ͨ��");
+						}
+					} else {
+						log.info(server + "����Լ��ͨ��");
+					}
+					Thread.sleep(1000 * DELAY_SECS_OF_TWO_SERVER_ALIVE_CHECK_ZOOKEEPER);
+				} catch (Exception e) {
+					log.error(server + "����Լ��ʧ�ܣ�ԭ����" + e.getMessage());
+				} finally {
+					if (null != sub)
+						sub.close();
+				}
+			}
+			Thread.sleep(5000);
+		}
+
+	}
+
+}
---
+++ src/main/common/toolkit/java/util/collection/SetUtil.java
@@ -0,0 +1,52 @@
+package common.toolkit.java.util.collection;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import common.toolkit.java.constant.SymbolConstant;
+import common.toolkit.java.util.StringUtil;
+
+/**
+ * 类说明: List相关工具类
+ * @author 银时 yinshi.nc@taobao.com
+ */
+public class SetUtil {
+
+	public static <T> Set<T> convertToSet(List<T> collection) {
+		if (CollectionUtil.isBlank(collection))
+			return CollectionUtil.emptySet();
+		return new HashSet<T>(collection);
+	}
+
+	/**
+	 * parse the string to Set
+	 * @param @param originalStr abc, def,helloword,myname
+	 * @param @param splitStr ,
+	 * @return Set<String>
+	 */
+	public static Set<String> parseSet(String originalStr, String splitStr) {
+		Set<String> set = new LinkedHashSet<String>();
+
+		if (StringUtil.isBlank(originalStr) || StringUtil.isBlank(splitStr))
+			return set;
+
+		for (String str : originalStr.split(splitStr)) {
+			if (StringUtil.isBlank(str))
+				continue;
+			set.add(StringUtil.trimToEmpty(str));
+		}
+		return set;
+	}
+
+	/**
+	 * parse the string to Set
+	 * @param @param originalStr abc, def,helloword,myname
+	 * @return Set<String>
+	 */
+	public static Set<String> parseSet(String originalStr) {
+		return parseSet(originalStr, SymbolConstant.COMMA);
+	}
+
+}
